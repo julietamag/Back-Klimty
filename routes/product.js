82999353254c173122a8f77ee79408ccf153b339
Javier <66645737+jalarg@users.noreply.github.com>
2023-03-07 15:55:34 -0300

    Revert "seed feature"

--- routes/product.js
+++ routes/product.js
@@ -1,24 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { Product, Artist } = require("../models");
-const charlesWhite = require("../config/seedProducts");
 
-router.get("/", (req, res, next) => {
-  Product.findAll()
-    .then((artists) => {
-      return res.send(artists);
-    })
-    .catch(next);
-});
+router.get('/', (req, res, next) => {
+    
+})
 
-router.post("/", (req, res, next) => {
-  Product.bulkCreate(
-    charlesWhite.map((artwork) => ({ ...artwork, artistId: 1 }))
-  )
-    .then((createdProducts) => {
-      return res.status(201).send(createdProducts);
-    })
-    .catch(next);
-});
 
-module.exports = router;
+module.exports = router
--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
-const envs = require("./config/_envs");
+const envs = require('./config/_envs');
 const morgan = require("morgan");
-const cors = require("cors");
-const { Artist, Product, Cart, User } = require("./models");
-const routes = require("./routes");
+const cors = require('cors');
+const {Artist, Product, Cart, User} = require('./models');
+const routes = require('./routes')
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-); // esta librería es para poder trabajar front con back en localhost
+app.use(cors({
+	origin: 'http://localhost:3000',
+	credentials: true
+})) // esta librería es para poder trabajar front con back en localhost
 app.use(morgan("dev"));
 app.use(express.json());
 
@@ -21,12 +19,12 @@ app.use("/api", routes);
 
 // ERROR MIDDLEWARE
 app.use(function (err, req, res, next) {
-  console.error(err, err.stack);
-  res.status(500).send(err);
+    console.error(err, err.stack);
+    res.status(500).send(err);
 });
 
-db.sync({ force: false }).then(() => {
-  app.listen(envs.PORT, () => console.log(`SERVER ON PORT: ${envs.PORT}`));
-});
-
-module.exports = app;
+db.sync({force: false}).then(() => {
+    app.listen(envs.PORT, () => console.log(`SERVER ON PORT: ${envs.PORT}`));
+  });
+  
+  module.exports = app;
