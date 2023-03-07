fc8a25cb21293bb880d55933c9de13b9f623c6f8
Julieta Magnago <julietamagnago@gmail.com>
2023-03-07 17:24:42 +0100

    add route to get artist profile by name search

--- routes/artist.js
+++ routes/artist.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const { Artist, Product } = require("../models");
 const router = express.Router();
 
+
+// GET all info about artist by name 
+// FRONT!! => deben agrear un body con el user input bajo la categoria 'input'
 router.get('/', (req, res, next) => {
-    
+    Artist.findAll({
+        where: {
+          [S.Op.or]: [
+            { name: { [S.Op.like]: `%${req.body.input}%` } },
+          ],
+        },
+        include: {
+            model: Product
+        }
+      })
+    .then((results) => {
+      if (!results) res.statusCode(404);
+      res.send(results);
+    })
+    .catch(next);
 })
 
 
