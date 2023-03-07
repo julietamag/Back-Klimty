95e259d0a598d7c94f3b3a4435db61f0b71c4e20
Julieta Magnago <julietamagnago@gmail.com>
2023-03-07 17:46:54 +0100

    add artist route to get its profile by id

--- routes/artist.js
+++ routes/artist.js
@@ -2,26 +2,22 @@ const express = require("express");
 const { Artist, Product } = require("../models");
 const router = express.Router();
 
-
-// GET all info about artist by name 
-// FRONT!! => deben agrear un body con el user input bajo la categoria 'input'
-router.get('/', (req, res, next) => {
-    Artist.findAll({
-        where: {
-          [S.Op.or]: [
-            { name: { [S.Op.like]: `%${req.body.input}%` } },
-          ],
-        },
-        include: {
-            model: Product
-        }
-      })
+// GET all info about artist by id
+// FRONT!! => deben enviar el id del artista solicitado por parametro
+router.get("/:id", (req, res, next) => {
+  Artist.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: {
+      model: Product,
+    },
+  })
     .then((results) => {
       if (!results) res.statusCode(404);
       res.send(results);
     })
     .catch(next);
-})
+});
 
-
-module.exports = router
+module.exports = router;
