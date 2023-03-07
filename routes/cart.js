4c5e28ae7e254f303a403c28da6031611ebb2f07
Nicholas <inchauspenico@gmail.com>
2023-03-07 17:37:03 -0300

    "agrege las rutas de user"

--- routes/cart.js
+++ routes/cart.js
@@ -1,10 +1,53 @@
 const express = require("express");
+const { Cart, Product } = require("../models");
+const nodemailer = require("nodemailer");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    
-})
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Cart.findByPk(id)
+    .then((cart) => {
+      res.status(200).send(cart);
+    })
+    .catch(() =>
+      res.status(200).send({ error: "the cart couldn't be loaded" })
+    );
+});
 
+router.post("/:cartId/:productId", (req, res) => {
+  const id = req.params.cartId;
+  const productId = req.params.productId;
+  Product.findByPk(productId)
+    .then((item) => {
+      let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false,
+        auth: {
+          user: "Klimty Ecommerce",
+          pass: "Klimty1234",
+        },
+      });
 
+      const message = {
+        from: "klimtyecommerce@gmail.com",
+        to: item.email,
+        subject: `Your purchased of ${item.name}, has been confirmed.`,
+        text: "Don't really have much to say",
+      };
 
-module.exports = router
+      transporter.sendMail(message, function (err, data) {
+        if (err) {
+          res.status(400).send({error: "failed to send the email"});
+        } else {
+          Cart.products.findByPk(productId).then((item) => {
+            item.destroy().then((data) => res.status(200).send(data));
+          }).catch(() => res.status(400).send({error: "failed to delete the purchased product"}))
+          res.status(200).send(data);
+        }
+      });
+    })
+    .catch(() => res.status(400).send({ error: "failed to send the email" }));
+});
+
+module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,51 @@
 const express = require("express");
+const { User } = require("../models")
+const {Op} = require("sequelize")
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.post('/', (req, res) => {
+    const { name, lastName, email, UID, isAdmin, address } = req.body;
+    User.create({ name, lastName, email, UID, isAdmin, address }).then((newUser) => {
+        res.status(201).send(newUser)
+    })
+    .catch((err) => res.status(400).send(err))
     
+})
+
+router.post('/login', (req, res) => {
+    User.findOrCreate({
+        where: {
+            [Op.and]: [
+            { UID: req.body.UID }, { email: req.body.email}
+        ]
+        }
+    }).then((logUser) => res.status(205).send(logUser))
+    .catch((err) => res.status(401).send({error: "password or email don't match"}))
+})
+
+router.put(`/edit/:id`, (req, res) => {
+    const {name, lastName, UID} = req.body
+    User.update({name: name, lastName: lastName, UID: UID}, {
+        where: { email: req.body.email}, returning: true, plain: true }).then((editUser) => res.status(201).send(editUser))
+    .catch(() => res.status(400).send({error: "failed to edit a"}))
+})
+
+router.get(`/:id`, (req, res) => {
+    const { id } = req.params
+    User.findOne({where: { id: id}}).then((user) => res.status(200).send(user))
+    .catch((err) => res.status(400).send(err))
+})
+
+
+router.delete("/:id/logout", (req, res) => {
+    (req.session) ? req.session.destroy((err => {
+        if (err) {
+          res.status(400).send('Unable to log out')
+        } else {
+          res.status(200).send('Logout successful')
+        }
+      })) :  res.end()
+
 })
 
 
