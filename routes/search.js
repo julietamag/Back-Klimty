72e3ecd06a878ae2928feebe3d1ec0b1ea7b856c
jalarg <jlema1990@gmail.com>
2023-03-13 21:30:37 -0300

    new search routes

--- routes/search.js
+++ routes/search.js
@@ -5,6 +5,28 @@ const S = require("sequelize");
 
 // GET all info about artist by name
 // FRONT!! pasar user input bajo query 'input'
+
+router.get("/artworks/:artist", (req, res, next) => {
+  const titleArtist = req.params.artist;
+  console.log("INFO BACK", titleArtist);
+  Artist.findOne({
+    where: {
+      title: titleArtist,
+    },
+  }).then((artist) => {
+    console.log("INFO BACK", artist);
+    Product.findAll({
+      where: {
+        artistId: artist.id,
+      },
+    })
+      .then((results) => {
+        res.send(results);
+      })
+      .catch(next);
+  });
+});
+
 router.get("/artist", (req, res, next) => {
   Artist.findAll({
     where: {
@@ -40,9 +62,34 @@ router.get("/product", (req, res, next) => {
     .catch(next);
 });
 
+// Find all categories
+
+router.get("/categories", (req, res, next) => {
+  Product.findAll()
+    .then((products) => {
+      const categories = products
+        .map((product) => {
+          console.log("informacion:", product.category);
+          return product.category;
+        })
+        .flat()
+        // chequear despues
+        .filter((category, index, self) => {
+          return index === self.indexOf(category);
+        });
+      console.log("QUE ES ESTO", categories);
+      return categories;
+    })
+    .then((categories) => {
+      return res.send(categories);
+    })
+
+    .catch(next);
+});
+
 // GET all info about products by category
 
-router.get("/:category", (req, res, next) => {
+router.get("/products/:category", (req, res, next) => {
   const category = req.params.category;
   Product.findAll({
     where: {
