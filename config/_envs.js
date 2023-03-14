766481c0208d8a97f9370bfdbd250f4a1ef10ed8
jalarg <jlema1990@gmail.com>
2023-03-14 19:51:35 -0300

    modificacion de envs

--- config/_envs.js
+++ config/_envs.js
@@ -1,6 +1,6 @@
 module.exports = {
-    DB_NAME: 'klimty',
-    PORT: 3003,
-    PASS: "Metro2033",
-    HOST: 'localhost'
-}
+  DB_NAME: "klimty",
+  PORT: 3001,
+  DB_PASSWORD: "",
+  HOST: "localhost",
+};
