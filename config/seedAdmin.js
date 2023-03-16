f80794b546476479738fccc68f25f82008adcb72
jalarg <jlema1990@gmail.com>
2023-03-16 10:33:17 -0300

    seedfix

--- config/seedAdmin.js
+++ config/seedAdmin.js
@@ -1,3 +1,9 @@
+// Edit with your email and your Firebase UID.
+// Information:
+// jlema1990@gmail.com: xKTy7Cs1JVQvDQVTgq9Omte4QAA3
+// inchauspenico@gmail.com: cInSrYKl4OXbsTDqvvnLy6B9HJH3
+// julietamagnago@gmail.com :bUvFqSdgJWUdIIBhOkkIwv62LFK2
+// xxdr0p3xx@gmail.com: EOAGqHqWGSPzbhYEyA3UoQxkOpi1git
 const seedAdmin = {
   name: "admin",
   lastName: "admin",
