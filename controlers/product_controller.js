cfaea812f73572eff879101016588db0448e90b1
NicholasInchauspe2 <inchauspenicholasar@gmail.com>
2023-03-17 14:10:03 -0300

    newservice/fixLogin

--- controlers/product_controller.js
+++ controlers/product_controller.js
@@ -50,26 +50,11 @@ exports.admin_by_product_id_delete = async (req, res, next) => {
 }
 
 exports.admin_add_post = async (req, res, next) => {
-  try {
-    const { name, price, description, category, photo_url, artistId } =
-      req.body;
-    const AdminUser = await User.findByPk(req.params.userId);
-    if (AdminUser.isAdmin === true) {
-      const newProduct = await Product.create({
-        name,
-        price,
-        description,
-        category,
-        photo_url,
-        artistId,
-      });
-      return res.status(201).send(newProduct);
-    } else {
-      return res.status(401).send({ error: "Your account is not admin" });
-    }
-  } catch (err) {
-    return next(err);
-  }
+  const userId = req.params.userId
+  const { artistId } = req.body
+  const body = req.body
+  const newProduct = await product_service.addProduct(userId, artistId, body)
+  return res.send(newProduct)
 };
 
 exports.admin_edit_product_put = async (req, res, next) => {
